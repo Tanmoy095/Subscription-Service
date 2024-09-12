b837a0d535efa67ef818dcbf6d0136eb1fda18fb
Tanmoy095 <adtanmoy95@gmail.com>
2024-09-12 18:08:04 +0600

    connect postgres initialize alex Edwards  scs session manager and redisStore

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,9 +1,23 @@
 package main
 
+import (
+	"database/sql"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/alexedwards/scs/redisstore"
+	"github.com/alexedwards/scs/v2"
+	"github.com/gomodule/redigo/redis"
+)
+
 const webport = "90"
 
 func main() {
 	//connect to database
+	db := initDB()
+	db.Ping()
 
 	//create sessions
 
@@ -17,4 +31,80 @@ func main() {
 
 	//listen for web connection
 
+}
+func initDB() *sql.DB {
+	// i will call another function just because
+	// i can try to connect to database repeatedly if necessary
+
+	conn := connectToDB()
+	if conn == nil {
+		log.Panic("cant connect to database")
+
+	}
+
+	return nil
+}
+func connectToDB() *sql.DB {
+	counts := 0
+	//i will try to connect to database fixed number of times
+	//if it fails after that time it dies
+
+	//connection string
+	dsn := os.Getenv("DSN")
+
+	for {
+		connection, err := openDB(dsn)
+		if err != nil {
+			log.Panicln("Posgtres not ready yet")
+
+		} else {
+			log.Println("connected to database")
+			return connection
+		}
+		if counts > 10 {
+			return nil
+		}
+		log.Print("Backing off for 1 second")
+		time.Sleep(time.Second * 1)
+	}
+}
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+
+	}
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+
+	}
+	return db, nil
+
+}
+
+// Initialize the session manager with Redis store
+func initSession() *scs.SessionManager {
+	session := scs.New()
+	session.Store = redisstore.New(initRedis()) // Use Redis store
+	session.Lifetime = 24 * time.Hour           // Set session lifetime to 24 hours
+	session.Cookie.Persist = true               // Make session cookie persistent
+	//it Configures the session cookie to persist in the user's browser even after the browser is closed.
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	//Sets the SameSite attribute to Lax, which allows the cookie to be sent with same-site requests and some cross-site requests.
+	session.Cookie.Secure = true // Cookie is only sent over HTTPS
+	// Ensures the cookie is only sent over HTTPS
+
+	return session
+}
+
+// Initialize the Redis connection pool
+func initRedis() *redis.Pool {
+	redisPool := &redis.Pool{
+		MaxIdle: 10, // Maximum number of idle connections
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial("tcp", os.Getenv("REDIS")) // Get Redis address from environment variable
+		},
+	}
+	return redisPool
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module subscription-service
 go 1.21.4
 
 require (
+	github.com/alexedwards/scs/redisstore v0.0.0-20240316134038-7e11d57e8885 // indirect
 	github.com/alexedwards/scs/v2 v2.8.0 // indirect
 	github.com/go-chi/chi/v5 v5.1.0 // indirect
+	github.com/gomodule/redigo v1.9.2 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.14.3 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
